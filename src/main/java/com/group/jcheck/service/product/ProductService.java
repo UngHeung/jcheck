49ecbbb41ef2888cdb12e4d29bf4b7c0af632d57
ungheung <t01047377477@gmail.com>
2024-06-29 17:08:44 +0900

    feat : 제품 등록 함수 추가

---
+++ src/main/java/com/group/jcheck/service/product/ProductService.java
@@ -0,0 +1,25 @@
+package com.group.jcheck.service.product;
+
+import com.group.jcheck.domain.product.Product;
+import com.group.jcheck.dto.product.request.CreateProductRequest;
+import com.group.jcheck.repository.product.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public String createProduct(CreateProductRequest request) {
+        if (productRepository.findByProductModelName(request.getProductModelName()).isPresent())
+            throw new IllegalArgumentException("이미 해당 모델명으로 등록된 제품이 있습니다..");
+        Product product = new Product(request);
+        productRepository.save(product);
+        return "제품이 정상적으로 등록되었습니다.";
+    }
+}
