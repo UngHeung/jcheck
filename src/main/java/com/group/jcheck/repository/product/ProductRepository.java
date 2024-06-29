83dd2d5550463c85b8039ec5623ec45a9f6360ee
ungheung <t01047377477@gmail.com>
2024-06-29 17:08:16 +0900

    fix : 모델명은 단일 검색으로 변경

--- src/main/java/com/group/jcheck/repository/product/ProductRepository.java
+++ src/main/java/com/group/jcheck/repository/product/ProductRepository.java
@@ -4,8 +4,9 @@ import com.group.jcheck.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByProductPetNameContaining(String productPatName);
-    List<Product> findByProductModelNameContaining(String productModelName);
+    Optional<Product> findByProductModelName(String productModelName);
 }
