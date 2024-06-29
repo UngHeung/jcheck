26347350384dca4c4c040558fbed85df3c3d083e
ungheung <t01047377477@gmail.com>
2024-06-30 00:27:15 +0900

    feat : 유저 이름, 연락처, 비밀번호 변경, 비밀번호 리셋 함수 추가

--- src/main/java/com/group/jcheck/service/user/UserService.java
+++ src/main/java/com/group/jcheck/service/user/UserService.java
@@ -1,9 +1,7 @@
 package com.group.jcheck.service.user;
 
 import com.group.jcheck.domain.user.User;
-import com.group.jcheck.dto.user.request.CreateUserRequest;
-import com.group.jcheck.dto.user.request.UpdateUserNameRequest;
-import com.group.jcheck.dto.user.request.UpdateUserPhoneNumberRequest;
+import com.group.jcheck.dto.user.request.*;
 import com.group.jcheck.dto.user.response.UserResponse;
 import com.group.jcheck.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
@@ -51,6 +49,31 @@ public class UserService {
 
     @Transactional
     public String updateUserPhoneNumber(UpdateUserPhoneNumberRequest request) {
+        User user = userRepository.findByUserId(request.getUserId())
+                .orElseThrow(IllegalArgumentException::new);
+        if (userRepository.findByUserPhoneNumber(request.getNewUserPhoneNumber()).isPresent())
+            throw new IllegalArgumentException("이미 등록된 핸드폰번호 입니다.");
+        user.updateUserPhoneNumber(request.getNewUserPhoneNumber());
+        return user.getUserName() + "님의 연락처가 " + user.getUserPhoneNumber() + "(으)로 정상적으로 변경되었습니다.";
+    }
 
+    @Transactional
+    public String UpdateUserPassword(UpdateUserPasswordRequest request) {
+        User user = userRepository.findByUserId(request.getUserId())
+                .orElseThrow(IllegalArgumentException::new);
+        if (user.getUserPassword().equals(request.getCurrentUserPassword()))
+            throw new IllegalArgumentException("비밀번호를 확인해주세요.");
+        user.updateUserPassword(request.getNewUserPassword());
+        return user.getUserName() + "님의 비밀번호가 정상적으로 변경되었습니다.";
+    }
+
+    @Transactional
+    public String ResetUserPassword(ResetUserPasswordRequest request) {
+        User user = userRepository.findByUserId(request.getUserId())
+                .orElseThrow(IllegalArgumentException::new);
+        if (!request.getSuperAdminPassword().equals("tempPw"))
+            throw new IllegalArgumentException("비밀번호를 확인해주세요.");
+        user.updateUserPassword("임시비밀번호");
+        return user.getUserName() + "님의 비밀번호가 정상적으로 초기화되었습니다. (" + "임시비밀번호" + ")"
     }
 }
