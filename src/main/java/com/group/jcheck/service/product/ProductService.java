06b4f71d9b85f4ce0035385e46205d3243fef247
ungheung <t01047377477@gmail.com>
2024-06-29 19:49:10 +0900

    feat : 제품 업로드, 제품 삭제 함수 추가

--- src/main/java/com/group/jcheck/service/product/ProductService.java
+++ src/main/java/com/group/jcheck/service/product/ProductService.java
@@ -2,6 +2,7 @@ package com.group.jcheck.service.product;
 
 import com.group.jcheck.domain.product.Product;
 import com.group.jcheck.dto.product.request.CreateProductRequest;
+import com.group.jcheck.dto.product.request.UpdateProductRequest;
 import com.group.jcheck.dto.product.response.ProductResponse;
 import com.group.jcheck.repository.product.ProductRepository;
 import org.springframework.stereotype.Service;
@@ -32,5 +33,35 @@ public class ProductService {
         return productRepository.findAll().stream()
                 .map(ProductResponse::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public String updateProduct(UpdateProductRequest request) {
+        Product product = productRepository.findByProductModelName(request.getProductModelName())
+                .orElseThrow(IllegalArgumentException::new);
+        if (productRepository.findByProductModelName(request.getNewProductModelName()).isPresent())
+            throw new IllegalArgumentException("이미 해당 모델명으로 등록된 제품이 있습니다.");
+        if (request.getNewProductModelName() != null)
+            product.updateProductModelName(request.getNewProductModelName());
+        if (request.getNewProductPetName() != null)
+            product.updateProductPetName(request.getNewProductPetName());
+        if (request.getNewProductMemory() != null)
+            product.updateProductMemory(request.getNewProductMemory());
+        if (request.getNewProductPrice() != null)
+            product.updateProductPrice(request.getNewProductPrice());
+        if (request.getNewProductManufacturer() != null)
+            product.updateProductManufacturer(request.getNewProductManufacturer());
+        if (request.getNewProductColors() != null)
+            product.updateProductColors(request.getNewProductColors());
+        return "제품 업데이트가 완료되었습니다.";
+    }
+
+    @Transactional
+    public String deleteProduct(String productModelName) {
+        Product product = productRepository.findByProductModelName(productModelName)
+                .orElseThrow(IllegalArgumentException::new);
+        String deleteProduct = product.getProductModelName();
+        productRepository.delete(product);
+        return deleteProduct + " 제품 삭제가 완료되었습니다.";
     }
 }
