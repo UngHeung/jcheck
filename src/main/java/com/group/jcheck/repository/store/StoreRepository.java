d437f388b454c719344cfe5e903460fe7a356a47
ungheung <t01047377477@gmail.com>
2024-06-29 22:38:38 +0900

    update : 판매점 코드로 검색하는 기능 추가

--- src/main/java/com/group/jcheck/repository/store/StoreRepository.java
+++ src/main/java/com/group/jcheck/repository/store/StoreRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
     Optional<Store> findByStoreName(String storeName);
+    Optional<Store> findByStoreCode(String storeCode);
 }
