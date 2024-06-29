09269e233dfb08f43c9c90c1fd72b930bdd93778
ungheung <t01047377477@gmail.com>
2024-06-30 00:09:25 +0900

    fix : 옵셔널 타입을 UserResponse에서 User로 변경

--- src/main/java/com/group/jcheck/repository/user/UserRepository.java
+++ src/main/java/com/group/jcheck/repository/user/UserRepository.java
@@ -1,12 +1,12 @@
 package com.group.jcheck.repository.user;
 
 import com.group.jcheck.domain.user.User;
-import com.group.jcheck.dto.user.response.UserResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserResponse, Long> {
-    Optional<UserResponse> findByUserId(String userId);
-    Optional<UserResponse> findByUserNameAndUserPhoneNumber(String userName, String userPhoneNumber);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
+    Optional<User> findByUserPhoneNumber(String userPhoneNumber);
+    Optional<User> findByUserNameAndUserPhoneNumber(String userName, String userPhoneNumber);
 }
