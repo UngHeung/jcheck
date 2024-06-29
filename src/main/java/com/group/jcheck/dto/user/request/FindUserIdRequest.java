c12f2cbf36eb9640c285ccb4acf4678e9d1cbfdf
ungheung <t01047377477@gmail.com>
2024-06-30 00:49:58 +0900

    feat : 유저 아이디 찾기 request 클래스 추가

---
+++ src/main/java/com/group/jcheck/dto/user/request/FindUserIdRequest.java
@@ -0,0 +1,9 @@
+package com.group.jcheck.dto.user.request;
+
+import lombok.Getter;
+
+@Getter
+public class FindUserIdRequest {
+    private String userName;
+    private String userPhoneNumber;
+}
