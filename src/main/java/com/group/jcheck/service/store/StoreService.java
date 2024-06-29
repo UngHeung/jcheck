f4aa282db40d186a00e3069b80bb5cc4478657fb
ungheung <t01047377477@gmail.com>
2024-06-29 22:12:18 +0900

    feat : 스토어 등록 함수 추가

--- src/main/java/com/group/jcheck/service/store/StoreService.java
+++ src/main/java/com/group/jcheck/service/store/StoreService.java
@@ -1,7 +1,24 @@
 package com.group.jcheck.service.store;
 
+import com.group.jcheck.domain.store.Store;
+import com.group.jcheck.dto.store.request.CreateStoreRequest;
+import com.group.jcheck.repository.store.StoreRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StoreService {
+    private final StoreRepository storeRepository;
+
+    public StoreService(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
+
+    @Transactional
+    public String createStore(CreateStoreRequest request) {
+        if (storeRepository.findByStoreName().isPresent())
+            throw new IllegalArgumentException("이미 해당 이름으로 등록된 판매점이 있습니다.");
+        storeRepository.save(new Store(request));
+        return "판매점이 정상적으로 등록되었습니다.";
+    }
 }
