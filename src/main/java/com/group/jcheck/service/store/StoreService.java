8cd7557046933b53a40426d999ca3077d0b9c829
ungheung <t01047377477@gmail.com>
2024-06-29 22:38:56 +0900

    feat : 판매점 업데이트, 삭제 함수 추가

--- src/main/java/com/group/jcheck/service/store/StoreService.java
+++ src/main/java/com/group/jcheck/service/store/StoreService.java
@@ -2,6 +2,8 @@ package com.group.jcheck.service.store;
 
 import com.group.jcheck.domain.store.Store;
 import com.group.jcheck.dto.store.request.CreateStoreRequest;
+import com.group.jcheck.dto.store.request.UpdateStoreCodeRequest;
+import com.group.jcheck.dto.store.request.UpdateStoreNameRequest;
 import com.group.jcheck.dto.store.response.StoreResponse;
 import com.group.jcheck.repository.store.StoreRepository;
 import org.springframework.stereotype.Service;
@@ -37,5 +39,36 @@ public class StoreService {
     public Store readStoreByStoreName(String storeName) {
         return storeRepository.findByStoreName(storeName)
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Transactional
+    public String updateStoreName(UpdateStoreNameRequest request) {
+        if (storeRepository.findByStoreName(request.getNewStoreName()).isPresent())
+            throw new IllegalArgumentException("이미 해당 이름으로 등록된 판매점이 있습니다.");
+        Store store = storeRepository.findByStoreName(request.getStoreName())
+                .orElseThrow(IllegalArgumentException::new);
+        String currentStoreName = request.getStoreName();
+        store.updateStoreName(request.getNewStoreName());
+        return currentStoreName + " 판매점 이름이 " + store.getStoreName() + "으로 변경되었습니다.";
+    }
+
+    @Transactional
+    public String updateStoreCode(UpdateStoreCodeRequest request) {
+        if (storeRepository.findByStoreName(request.getNewStoreCode()).isPresent())
+            throw new IllegalArgumentException("이미 해당 코드로 등록된 판매점이 있습니다.");
+        Store store = storeRepository.findByStoreCode(request.getStoreCode())
+                .orElseThrow(IllegalArgumentException::new);
+        String currentStoreCode = request.getStoreCode();
+        store.updateStoreCode(request.getNewStoreCode());
+        return currentStoreCode + " 판매점 코드가 " + store.getStoreCode() + "으로 변경되었습니다.";
+    }
+
+    @Transactional
+    public String deleteStore(String storeName) {
+        Store store = storeRepository.findByStoreName(storeName)
+                .orElseThrow(IllegalArgumentException::new);
+        String deleteStoreName = store.getStoreName();
+        storeRepository.delete(store);
+        return deleteStoreName + " 판매점 삭제가 완료되었습니다.";
     }
 }
