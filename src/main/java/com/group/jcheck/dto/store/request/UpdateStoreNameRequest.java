d6d168cbe2cbcc5771ba29644524115c7900e3fd
ungheung <t01047377477@gmail.com>
2024-06-29 22:38:08 +0900

    feat : 스토어 업데이트 request 함수 추가

---
+++ src/main/java/com/group/jcheck/dto/store/request/UpdateStoreNameRequest.java
@@ -0,0 +1,9 @@
+package com.group.jcheck.dto.store.request;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateStoreNameRequest {
+    private String storeName;
+    private String newStoreName;
+}
