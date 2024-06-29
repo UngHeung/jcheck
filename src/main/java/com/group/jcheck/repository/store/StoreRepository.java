2a2b3bb1b2f893fc959ab71f77df8f0ba5136e75
ungheung <t01047377477@gmail.com>
2024-06-29 22:22:49 +0900

    feat : 판매점 전체목록, 단일 목록 이름으로 불러오기 함수 추가

--- src/main/java/com/group/jcheck/repository/store/StoreRepository.java
+++ src/main/java/com/group/jcheck/repository/store/StoreRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
-    Optional<Store> findByStoreName();
+    Optional<Store> findByStoreName(String storeName);
 }
--- src/main/java/com/group/jcheck/service/store/StoreService.java
+++ src/main/java/com/group/jcheck/service/store/StoreService.java
@@ -2,9 +2,13 @@ package com.group.jcheck.service.store;
 
 import com.group.jcheck.domain.store.Store;
 import com.group.jcheck.dto.store.request.CreateStoreRequest;
+import com.group.jcheck.dto.store.response.StoreResponse;
 import com.group.jcheck.repository.store.StoreRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StoreService {
@@ -16,9 +20,22 @@ public class StoreService {
 
     @Transactional
     public String createStore(CreateStoreRequest request) {
-        if (storeRepository.findByStoreName().isPresent())
+        if (storeRepository.findByStoreName(request.getStoreName()).isPresent())
             throw new IllegalArgumentException("이미 해당 이름으로 등록된 판매점이 있습니다.");
         storeRepository.save(new Store(request));
         return "판매점이 정상적으로 등록되었습니다.";
+    }
+
+    @Transactional
+    public List<StoreResponse> readStores() {
+        return storeRepository.findAll().stream()
+                .map(StoreResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Store readStoreByStoreName(String storeName) {
+        return storeRepository.findByStoreName(storeName)
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
