6f75955f3410576fc5eef19670e976f8b0e53f77
ungheung <t01047377477@gmail.com>
2024-06-29 16:25:15 +0900

    feat : 핸드폰 entity class 추가, 업데이트 함수 생성

---
+++ src/main/java/com/group/jcheck/domain/product/Product.java
@@ -0,0 +1,43 @@
+package com.group.jcheck.domain.product;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column(nullable = false, length = 40)
+    private String productPetName;
+    @Column(nullable = false, length = 20)
+    private String productModelName;
+    @Column(nullable = false)
+    private Integer productMemory;
+    @Column(nullable = false)
+    private Integer productPrice;
+    @Column(length = 10)
+    private String productManufacturer;
+    @Column(length = 100)
+    private String productColors;
+
+    public Product() {};
+
+    public product() {
+
+    }
+
+    public void updateProductPrice(Integer newProductPrice) {
+        this.productPrice = newProductPrice;
+    }
+
+    public void updateProductManufacturer(String newProductManufacturer) {
+        this.productManufacturer = newProductManufacturer;
+    }
+
+    public void updateProductColor(String newProductColors) {
+        this.productColors = newProductColors;
+    }
+}
