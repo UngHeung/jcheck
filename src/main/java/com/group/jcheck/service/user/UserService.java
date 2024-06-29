7180d2e221751671b40f94e20c4a080d88a3d3ee
ungheung <t01047377477@gmail.com>
2024-06-30 00:17:14 +0900

    feat : 유저 이름 업데이트 함수 추가

--- src/main/java/com/group/jcheck/service/user/UserService.java
+++ src/main/java/com/group/jcheck/service/user/UserService.java
@@ -1,2 +1,56 @@
-package com.group.jcheck.service.user;public class UserService {
+package com.group.jcheck.service.user;
+
+import com.group.jcheck.domain.user.User;
+import com.group.jcheck.dto.user.request.CreateUserRequest;
+import com.group.jcheck.dto.user.request.UpdateUserNameRequest;
+import com.group.jcheck.dto.user.request.UpdateUserPhoneNumberRequest;
+import com.group.jcheck.dto.user.response.UserResponse;
+import com.group.jcheck.repository.user.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.channels.IllegalChannelGroupException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public String createUser(CreateUserRequest request) {
+        if (userRepository.findByUserId(request.getUserId()).isPresent())
+            throw new IllegalArgumentException("이미 등록된 아이디 입니다..");
+        if (userRepository.findByUserPhoneNumber(request.getUserPhoneNumber()).isPresent())
+            throw new IllegalArgumentException("이미 등록된 핸드폰번호 입니다.");
+        userRepository.save(new User(request));
+        return "사용자가 정상적으로 등록되었습니다.";
+    }
+
+    @Transactional
+    public List<UserResponse> readUsers() {
+        return userRepository.findAll().stream()
+                .map(UserResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public String updateUserName(UpdateUserNameRequest request) {
+        User user = userRepository.findByUserId(request.getUserId())
+                .orElseThrow(IllegalChannelGroupException::new);
+        if (!request.getSuperAdminPassword().equals("tempPw"))
+            throw new IllegalArgumentException("비밀번호를 확인해주세요.");
+        String currentUserName = user.getUserName();
+        user.updateUserName(request.getNewUserName());
+        return currentUserName + " 유저 이름이 " + user.getUserName() + "으로 변경되었습니다.";
+    }
+
+    @Transactional
+    public String updateUserPhoneNumber(UpdateUserPhoneNumberRequest request) {
+
+    }
 }
