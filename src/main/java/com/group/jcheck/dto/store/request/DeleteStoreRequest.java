58871df352de60ee4cab32dbb9c1fb35180ff26a
ungheung <t01047377477@gmail.com>
2024-06-29 22:25:17 +0900

    feat : 판매점 삭제 request 클래스 추가

--- src/main/java/com/group/jcheck/dto/store/request/DeleteStoreRequest.java
+++ src/main/java/com/group/jcheck/dto/store/request/DeleteStoreRequest.java
@@ -1,4 +1,9 @@
 package com.group.jcheck.dto.store.request;
 
+import lombok.Getter;
+
+@Getter
 public class DeleteStoreRequest {
+    private String storeName;
+    private String superAdminPassword;
 }
