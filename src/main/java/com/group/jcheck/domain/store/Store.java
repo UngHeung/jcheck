2160f848b43bbfd0993b9bb1b5a51ad235ff8439
ungheung <t01047377477@gmail.com>
2024-06-29 22:02:35 +0900

    feat : store entity 클래스 추가, 업데이트 함수 추가

--- src/main/java/com/group/jcheck/domain/store/Store.java
+++ src/main/java/com/group/jcheck/domain/store/Store.java
@@ -1,9 +1,34 @@
 package com.group.jcheck.domain.store;
 
+import com.group.jcheck.dto.store.request.CreateStoreRequest;
+import com.group.jcheck.dto.store.response.StoreResponse;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+    private String storeName;
+    private String storeCode;
+
+    public Store() {}
+
+    public Store(CreateStoreRequest request) {
+        this.storeName = request.getStoreName();
+        this.storeCode = request.getStoreCode();
+    }
+
+    public void updateStoreName(String newStoreName) {
+        this.storeName = newStoreName;
+    }
+
+    public void updateStoreCode(String newStoreCode) {
+        this.storeCode = newStoreCode;
+    }
 }
