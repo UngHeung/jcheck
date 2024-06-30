3601d3eb5062d7c1837b7e1bd29b4d384ea43a67
ungheung <t01047377477@gmail.com>
2024-06-30 18:56:33 +0900

    feat : 비밀번호 암호와를 위한 세터 설정

--- src/main/java/com/group/jcheck/dto/admin/request/CreateAdminRequest.java
+++ src/main/java/com/group/jcheck/dto/admin/request/CreateAdminRequest.java
@@ -1,11 +1,13 @@
 package com.group.jcheck.dto.admin.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class CreateAdminRequest {
     private String adminId;
     private String adminName;
+    @Setter
     private String adminPassword;
     private String superAdminPassword;
 }
