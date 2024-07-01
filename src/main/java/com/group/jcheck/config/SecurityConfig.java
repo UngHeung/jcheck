c8731adac8f57c39d76eb9dfa384bc64686fb7d2
ungheung <t01047377477@gmail.com>
2024-07-01 13:52:44 +0900

    feat : 로그인 필터 추가, 시큐리티 컨피그 내 설정 추가

--- src/main/java/com/group/jcheck/config/SecurityConfig.java
+++ src/main/java/com/group/jcheck/config/SecurityConfig.java
@@ -1,17 +1,28 @@
 package com.group.jcheck.config;
 
+import com.group.jcheck.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
         security
@@ -25,12 +36,18 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                )
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
         return security.build();
     }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/group/jcheck/domain/jwt/JwtToken.java
+++
@@ -1,7 +0,0 @@
-package com.group.jcheck.domain.jwt;
-
-public class JwtToken {
-    private String grantToken;
-    private String accessToken;
-    private String refreshToken;
-}
